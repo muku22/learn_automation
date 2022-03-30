532f8b88f6aada8760e9c28694f732a2fb1c4894
Amrita <ask4am11@gmail.com>
2022-03-30 14:59:14 +0530

    Factorial

---
+++ src/main/java/LoginTest2.java
@@ -0,0 +1,15 @@
+public class LoginTest2 {
+    public static void main(String[] args) {
+
+        class Factorial{
+            public void main(String args[]){
+                int i,fact=1;
+                int n=5;
+                for(i=1;i<=n;i++){
+                    fact=fact*i;
+                }
+                System.out.println("Factorial of "+n+" is: "+fact);
+            }
+        }
+    }
+}
