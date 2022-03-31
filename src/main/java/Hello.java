e53cd3364b9d15da2f25c7ab5b8afaf2e8b1b8e4
Amrita <ask4am11@gmail.com>
2022-03-31 18:16:37 +0530

    Sample file

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main (String[] args) {
+
+        System.out.println("Hello Java file");
+        System.out.println("Welcome to Java Programming");
+
+    }
+}
