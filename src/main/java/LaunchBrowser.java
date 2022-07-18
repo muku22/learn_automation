59f341986eea556c18c4d97b983d46f9f113daba
mukesh <22ymukesh@gmail.com>
2022-07-18 21:30:20 +0530

    simply for fun
    Signed-off-by: muku2206 <muku2206@gmail.com>

--- src/main/java/LaunchBrowser.java
+++ src/main/java/LaunchBrowser.java
@@ -1,5 +1,6 @@
 public class LaunchBrowser {
     public static void main(String[] args) {
+        System.out.println("todays date is "+java.time.LocalDate.now());
         System.out.println("Browser run successfully");
     }
 }
